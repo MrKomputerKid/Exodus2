f5e1f24386944f7b8c0187e65f9572baa5701abc
Craig Wessel <craigwessel9@gmail.com>
2024-01-14 01:27:56 -0800

    Typo.

--- main.py
+++ main.py
@@ -486,7 +486,7 @@ async def quote(interaction):
 @tree.command(name='setlocation', description='Set your preferred location')
 async def setlocation(interaction, *, location: str, state_province: str, country: 'US'):
     pool, connection = await connect_to_db()
-    full_location = f"{location}, {state_province}, {country}" if state_province else location {country}
+    full_location = f"{location}, {state_province}, {country}" if state_province else f"{location}, {country}"
     await set_user_location(interaction.user.id, full_location, pool)
     await pool.release(connection)
     await interaction.response.send_message(f'Your location has been set to {location}.')
