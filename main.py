5d246c771330748c3e29afe30e5d8c23f47073dc
Craig Wessel <craigwessel9@gmail.com>
2024-01-14 04:03:31 -0800

    Weather fix round 13?

--- main.py
+++ main.py
@@ -436,6 +436,11 @@ async def get_most_populous_location(location: str, state_province: str, country
             # Check if state_result is 'None' (string) or None (NoneType)
             if state_result and state_result.lower() != 'none':
                 return f'{city}, {state_result}, {country_code}'
+            
+            # Check if there's state information in the OpenCage results
+            state_result_oc = opencage_data['results'][0]['components'].get('state', state_province)
+            if state_result_oc and state_result_oc.lower() != 'none':
+                return f'{city}, {state_result_oc}, {country_code}'
 
     # Return the original location with state_province and country codes
     if state_province and country:
@@ -444,6 +449,7 @@ async def get_most_populous_location(location: str, state_province: str, country
         return f'{location}, {country}'
     else:
         return location
+
 
 # Remind Me Command
 
