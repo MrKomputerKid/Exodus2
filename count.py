e27bde6a06978da2aaf37c96d3d7f727c6566603
Craig Wessel <craigwessel9@gmail.com>
2024-02-24 22:49:06 -0800

    revert to tree.command. Stupid bing chat.

--- count.py
+++ count.py
@@ -1,6 +1,7 @@
 import logging
 import discord
-from discord.ext import commands, tasks
+from discord import app_commands
+from discord.ext import tasks
 
 logging.basicConfig(level=logging.DEBUG)
 discord_logger = logging.getLogger('discord')
@@ -8,43 +9,46 @@ discord_logger.setLevel(logging.DEBUG)
 
 intents = discord.Intents.all()
 intents.members = True
-bot = commands.Bot(intents=intents)
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 counting_channel_id = 1211195326363336765  # Replace with the ID of your counting channel
 debug_channel_id = 1196711795906318437    # Replace with the ID of your debug channel
 target_number = 0
 
-@bot.slash_command(name='count', description='Start a counting game', guild_ids=[...])
+@tree.command(name='count', description='Start a counting game')
 async def count(interaction, number: int):
     global target_number
     target_number = number
     embed = discord.Embed(title="Counting Game", color=discord.Color.blurple())
-    embed.add_field(name="Instructions", value=f"Let's count to {target_number:,}! Start from 1.", inline=False)
+    embed.add_field(name="Instructions", value=f"Let's count to {target_number}! Start from 1.", inline=False)
     await interaction.response.send_message(content="@here", embed=embed)
 
 
-@bot.event
+@client.event
 async def on_message(message):
     global target_number
-    if message.author == bot.user:
+    if message.author == client.user:
         return
+
+
 
     if message.channel.id == counting_channel_id or debug_channel_id:
         try:
             count = int(message.content)
             if count == target_number:
-                message.channel.send(f'Congratulations! We\'ve reached {target_number:,}!')
+                await message.channel.send(f'Congratulations! We\'ve reached {target_number}!')
                 return
-            elif count != 1 and count != int(message.reference.cached_message.content) + 1:
-                message.channel.send(f'FUCK! {message.author.mention} Screwed up! Let\'s start over from 1!')
+            elif count != 1 and count != int(message.reference.resolved.content) + 1:
+                await message.channel.send(f'FUCK! {message.author.mention} Screwed up! Let\'s start over from 1!')
                 return
         except ValueError:
             pass
 
-    await bot.process_commands(message)
+    await client.process_commands(message)
 
 
 @count.error
 async def count_error(ctx, error):
-    if isinstance(error, commands.BadUnionArgument):
-        await ctx.interaction.response.send_message('Please enter a valid number to count to.', ephemeral=True)
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Please enter a valid number to count to.')
