e43d247711376d27ebf22314a227124849dfb79c
Craig Wessel <craigwessel9@gmail.com>
2024-02-24 22:49:35 -0800

    typo

--- count.py
+++ count.py
@@ -1,7 +1,7 @@
 import logging
 import discord
 from discord import app_commands
-from discord.ext import tasks
+from discord.ext import tasks, commands
 
 logging.basicConfig(level=logging.DEBUG)
 discord_logger = logging.getLogger('discord')
