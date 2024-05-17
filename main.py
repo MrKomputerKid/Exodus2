681331559ca07bbf41c4e10fb96d7fe67afb3a9d
Craig J. Wessel <craigwessel9@gmail.com>
2024-05-17 15:03:15 -0700

    Fixed errors relating to Discord.py 2.0

--- main.py
+++ main.py
@@ -47,9 +47,9 @@ tree.add_command(quote)
 tree.add_command(eightball)
 
 # Register the error handlers
-client.add_listeners(on_error)
-client.add_listeners(on_command_error)
-client.add_listeners(handle_error)
+client.add_event_handler(on_error)
+client.add_event_handler(on_command_error)
+client.add_event_handler(handle_error)
 
 # Connect to the MariaDB database.
 
